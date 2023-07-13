0477ac9701ccc55d92973387f18a16c8f9e0ee58
Edwin Cedillo <wincedbarz@gmail.com>
2023-07-13 01:48:14 +0000

    Cambios Huespedes

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/huespedes')
-def about():
-    return render_template('huespedes.html')
-
 
 @app.route('/personas')
 def personas():
@@ -34,6 +30,11 @@ def add():
     personaList.append({"nombre": nombre, "apellido": apellido, "edad": edad})
 
     return redirect(url_for('personas'))
+
+@app.route('/huespedes')
+def huespedes():
+    responseHabitaciones = requests.get('https://utpl-interoperabilidad-ejercicio1.onrender.com/v1_0/huesped')
+    return render_template('huespedes.html', huespedesl=responseHabitaciones.json())
 
 if __name__ == '__main__':
     app.run(debug=True)
